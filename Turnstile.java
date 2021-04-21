edd92f037db279e2d347f4a344bf74bc7e2ece19
Nikidhan22 <82935230+Nikidhan22@users.noreply.github.com>
2021-04-21 23:21:13 +0800

    Add files via upload

--- Turnstile.java
+++ Turnstile.java
@@ -1,21 +1,48 @@
-package com.company;
+package Museum;
 
 
 import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Turnstile {
 
+    private final Lock lock;
+    private final Condition fullCondition;
 
-    public static String getRandom() {
-        String[] gateName = {"NET1", "NET2", "NET3", "NET4", "SET1", "SET2", "SET3", "SET4", "EET1", "EET2", "EET3", "EET4", "WET1", "WET2", "WET3", "WET4",};
+    public Turnstile() {
+        lock = new ReentrantLock();
+        fullCondition = lock.newCondition();
+    }
+
+    public static String getEntranceGate() {
+        try{
+            lock.lock();
+            while(list.isEmpty()){
+                System.out.println("Stack is empty. Waiting..");
+                emptyCondition.await(1, TimeUnit.SECONDS);
+            }
+            Integer o =  list.pop();
+            fullCondition.signalAll();
+            return o;
+        } finally {
+            lock.unlock();
+        }
+        String[] gateName = {"NET1", "NET2", "NET3", "NET4", "SET1", "SET2", "SET3", "SET4"};
+        return (gateName[new Random().nextInt(gateName.length)]);
+    }
+
+    public static String getExitGate() {
+        String[] gateName = {"EET1", "EET2", "EET3", "EET4", "WET1", "WET2", "WET3", "WET4"};
         return (gateName[new Random().nextInt(gateName.length)]);
     }
 
     public void enter(String current_time, String ticketID, long duration){
-        System.out.println(current_time+" Ticket "+ticketID+" entered through "+getRandom()+". Staying for "+duration+" minutes.");
+        System.out.println(current_time+" Ticket "+ticketID+" entered through "+getEntranceGate()+". Staying for "+duration+" minutes.");
     }
 
     public void exit(String current_time,String ticketID){
-        System.out.println(current_time+ " Ticket "+ticketID+" exited through "+getRandom());
+        System.out.println(current_time+ " Ticket "+ticketID+" exited through "+getExitGate());
     }
 }
