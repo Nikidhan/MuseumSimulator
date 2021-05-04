2b675e009659e90a71d44d550021f4b7c64b507a
GeekDog99 <62506545+GeekDog99@users.noreply.github.com>
2021-05-05 01:10:40 +0800

    Update Turnstile.java

--- src/com/company/Turnstile.java
+++ src/com/company/Turnstile.java
@@ -1,6 +1,11 @@
+/**
+ * The Turnstile.java create an object that act as an environment 
+ * have 2 entrance and 2 exit, each of them have 4 turnstiles.
+ */
 package com.company;
-
-
+/**
+ * @author Aiman, Zikri, Ahlami, Nik
+ */
 import java.util.Random;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
@@ -12,32 +17,57 @@ public class Turnstile {
     private final Condition fullCondition;
     Timer timer;
 
+    /**
+     * A Turnstile constructor that is called in the Main to create Turnstile 
+     * object that used the Timer class to print out the time when visitors 
+     * enter and exit 
+     * @param timer 
+     */
     public Turnstile(Timer timer) {
         this.timer = timer;
         lock = new ReentrantLock();
         fullCondition = lock.newCondition();
     }
 
+    /**
+     * Method thar get the visitor's entrance gate and turnstile that randomly 
+     * assign to every customer.
+     * @return String
+     */
     public String getEntranceGate() {
         String[] gateName = {"NET1", "NET2", "NET3", "NET4", "SET1", "SET2", "SET3", "SET4"};
         return (gateName[new Random().nextInt(gateName.length)]);
     }
 
+    /**
+     * Method thar get the visitor's exit gate and turnstile that randomly 
+     * assign to every customer.
+     * @return String
+     */
     public static String getExitGate() {
         String[] gateName = {"EET1", "EET2", "EET3", "EET4", "WET1", "WET2", "WET3", "WET4"};
         return (gateName[new Random().nextInt(gateName.length)]);
     }
 
+    /**
+     * Method that prevent visitor to enter any turnstiles when the Museum current
+     * capacity reached the maximum current capacity. Once the current capacity 
+     * is less than the maximum current capacity, the visitor allow to enter and print 
+     * the current time of entrance, ticket ID, entrance gate and the duration.
+     * @param ticketID
+     * @param duration
+     * @param museum
+     * @throws InterruptedException 
+     */
     public void enter(String ticketID, long duration,Museum museum) throws InterruptedException{
+        //Once visitor enter, it will be lock to check whether the capacity exceed 
+        //the limit. While the capcity exceeded the limit, the system will await 
+        //for signalAll which indicate there visitor exit to allow the visitor to 
+        //enter.
         try{
             lock.lock();
             while(museum.getCurrentCapacity()==museum.getMaxCurrentCapacity()){
-//                System.out.println("Masuk "+timer.current_time);
-//                if(timer.current_time.charAt(1)=='9'){
-//                    fullCondition.signalAll();
-//                    System.out.println("Masuk");
-//                }
-                fullCondition.await();
+                fullCondition.await();//Wait for signalAll once visitor exit and new visitor to enter.
             }
             System.out.println(timer.current_time+" Ticket "+ticketID+" entered through "+getEntranceGate()+". Staying for "+duration+" minutes.");
         }finally{
@@ -46,11 +76,19 @@ public class Turnstile {
 
     }
 
+    /**
+     * Method that keep track of visitor that exit, print the current time of exit, 
+     * ticket ID and the exit gate. Then, it will signalAll to the enter method to
+     * allow visitor to enter.
+     * @param ticketID
+     * @param museum
+     * @throws InterruptedException 
+     */
     public void exit(String ticketID,Museum museum) throws InterruptedException{
         try{
             lock.lock();
             System.out.println(timer.current_time+ " Ticket "+ticketID+" exited through "+getExitGate());
-            fullCondition.signalAll();
+            fullCondition.signalAll();//signal .await to allow next visitor to enter.
         } finally {
             lock.unlock();
         }
