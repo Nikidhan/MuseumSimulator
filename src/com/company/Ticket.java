e294be5976a693ddbb5a6a4a0aeb1cbe98f553ac
GeekDog99 <62506545+GeekDog99@users.noreply.github.com>
2021-05-05 21:57:04 +0800

    Update Ticket.java

--- src/com/company/Ticket.java
+++ src/com/company/Ticket.java
@@ -1,6 +1,12 @@
+/**
+ * Ticket.java simulate visitor enter the museum, view the gallery for a time 
+ * being and exit via turnstile 
+ */
 package com.company;
 import java.util.*;
-
+/**
+ * @author Aiman, Zikri, Ahlami, Nik
+ */
 public class Ticket extends Thread{
 
     String ticketID;
@@ -9,6 +15,14 @@ public class Ticket extends Thread{
     Turnstile turnstile;
     Timer timer;
 
+    /**
+     * A Ticket constructor that is called in the TicketCounter to create Ticket 
+     * object.
+     * @param timer
+     * @param museum
+     * @param turnstile
+     * @param timestamp 
+     */
     public Ticket (Timer timer, Museum museum, Turnstile turnstile, long timestamp){
         this.museum = museum;
         this.turnstile = turnstile;
@@ -24,21 +38,24 @@ public class Ticket extends Thread{
 
     @Override
     public void run() {
-        //simulate people enter the museum and view the gallery for a time being and exit via turnstile
         Random rand = new Random();
-        int duration = rand.nextInt(150-50+1) + 50;
+        int duration = rand.nextInt(150-50+1) + 50;// Duration visitor inside the museum are randomized
         int hours = duration/60;
         int minutes = duration%60;
 
         try {
+            //No visitor enter at 0800
             while(timer.curremt_time.charAt(1)=='8'){
                 
             }    
             turnstile.enter(ticketID,duration,museum);
             museum.visitor_enter();
+            
+            // To calculate the exit time 
             int exit_hour = timer.getCurrentHour() + hours;
             int exit_minute = timer.getCurrentMin() + minutes;
 
+            // Calculate if the minute exceed 60 min = 1 hour
             if(exit_minute>59){
                 int add_hour=0;
                 int new_min=0;
@@ -50,10 +67,13 @@ public class Ticket extends Thread{
 
             while(true){
                 Thread.sleep(200);
+                // Visitor exit when the current time reach the exit time of the visitor
                 if(exit_hour==timer.getCurrentHour()&&exit_minute==timer.getCurrentMin()){
                     turnstile.exit(ticketID, museum);
                     museum.visitor_exit();
                     break;
+                    
+                // Forcing all visitors inside the museum exit.    
                 } else if(timer.getCurrentHour()==17&&timer.getCurrentMin()>55){
                     System.out.println(ticketID + "  is exiting because museum is closing");
                     turnstile.exit(ticketID, museum);
