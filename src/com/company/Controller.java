02fd2c888c5bf6c02370e385227be1499b3a6f97
MUHAMMAD ZIKRI BIN MOHD YUNAN <45089636+DrPolymath@users.noreply.github.com>
2021-06-10 13:06:00 +0800

    Update Controller.java

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -4,7 +4,11 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,8 +19,16 @@ public class Controller implements Initializable {
     public Text timerTxt;
     public Text ticketRemainingTxt;
     public TextField setDailyVisitorLimit, setCapacityLimit, setTicketLimit;
-
-
+    public Circle openCircle;
+    public Circle closingCircle;
+    public Circle closedCircle;
+    public TextArea netGate;
+    public TextArea setGate;
+    public TextArea eetGate;
+    public TextArea wetGate;
+    public TextArea ticketsSellingLog;
+    public ListView museum;
+    public ListView waitingList;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -25,6 +37,51 @@ public class Controller implements Initializable {
 
     public void setTimerTxt(String time){
         timerTxt.setText(time);
+    }
+
+    public void changeMuseumStatus (String status){
+        if (status.equals("Open")) {
+            closedCircle.setFill(Paint.valueOf("#ffffff"));
+            openCircle.setFill(Paint.valueOf("#2ecc71"));
+        } else if (status.equals("Closing")) {
+            openCircle.setFill(Paint.valueOf("#ffffff"));
+            closingCircle.setFill(Paint.valueOf("#f1c40f"));
+        } else {
+            closingCircle.setFill(Paint.valueOf("#ffffff"));
+            closedCircle.setFill(Paint.valueOf("#e74c3c"));
+        }
+    }
+
+    public void setTicketSellingLogStatement(String logStatement){
+        ticketsSellingLog.appendText("\n"+logStatement);
+    }
+
+    public void enterNETGate(String visitor){
+        netGate.appendText("\n"+visitor + " enter.");
+        museum.getItems().add(visitor);
+    }
+
+    public void enterSETGate(String visitor){
+        setGate.appendText("\n"+visitor + " enter.");
+        museum.getItems().add(visitor);
+    }
+
+    public void exitEETGate(String visitor){
+        eetGate.appendText("\n"+visitor + " exit.");
+        museum.getItems().remove(visitor);
+    }
+
+    public void exitWETGate(String visitor){
+        wetGate.appendText("\n"+visitor + " exit.");
+        museum.getItems().remove(visitor);
+    }
+
+    public void waiting(String visitor){
+        waitingList.getItems().add(visitor);
+    }
+
+    public void stopWaiting(String visitor){
+        waitingList.getItems().remove(visitor);
     }
 
     public void setTicketRemainingTxtTxt(int ticket){
@@ -68,7 +125,7 @@ public class Controller implements Initializable {
 
         Timer timer = new Timer(this);
         Museum museum = new Museum(dailyVisitorLimit,capacityLimit);
-        Turnstile turn = new Turnstile(timer);
+        Turnstile turn = new Turnstile(timer,this);
         TicketCounter t1 = new TicketCounter(timer,museum,turn,this,ticketLimit);
         timer.start();
         t1.start();
