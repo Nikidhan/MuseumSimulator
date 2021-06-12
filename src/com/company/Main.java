7fc9edea0b12985bf63508e9daa261e2c6d8655d
MUHAMMAD ZIKRI BIN MOHD YUNAN <45089636+DrPolymath@users.noreply.github.com>
2021-06-12 18:20:49 +0800

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -39,7 +39,7 @@ public class Main extends Application{
 //        System.out.println("Enter the number of tickets:");
 //        int totalTicket = read.nextInt();
 //
-//        com.company.Museum museum = new com.company.Museum(max_current_capacity,max_total_capacity);
+//        Museum museum = new Museum(max_current_capacity,max_total_capacity);
 //        Timer timer = new Timer();
 //        Turnstile turn = new Turnstile(timer);
 //        TicketCounter t1 = new TicketCounter(timer,museum,turn,totalTicket);
