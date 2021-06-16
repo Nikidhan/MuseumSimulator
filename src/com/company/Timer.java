234da212c332d3a8ae103a0190299099f2709bb1
GeekDog99 <62506545+GeekDog99@users.noreply.github.com>
2021-06-16 23:03:14 +0800

    Update Timer.java

--- src/com/company/Timer.java
+++ src/com/company/Timer.java
@@ -5,87 +5,103 @@
  */
 package com.company;
 
+import javafx.application.Platform;
+
 /**
  * @author Aiman, Zikri, Ahlami, Nik
  */
 public class Timer extends Thread{
 
-        String current_time;
-        private int openTime;
-        int current_hour;
-        int current_mins;
-        Controller controller;
-                
-        /**
-         * Timer constructor that is called in the Main to create Timer object 
-         * that run the time printing and also notify the museum is opened,
-         * closing and closed.
-         */
-        public Timer(Controller controller){
-            this.controller = controller;
-        }
-        
-        /**
-         * Method that get only the current hour.
-         * @return current_hour
-         */
-        public int getCurrentHour(){
-            return current_hour;
-        }
-        
-        /**
-         * Method that get only the current minutes.
-         * @return current_mins
-         */
-        public int getCurrentMin(){
-            return current_mins;
-        }
+    String current_time;
+    private int openTime;
+    int current_hour;
+    int current_mins;
+    Controller controller;
+    Museum museum;
 
-        public String getCurrentTime(){
+    /**
+     * Timer constructor that is called in the Main to create Timer object
+     * that run the time printing and also notify the museum is opened,
+     * closing and closed.
+     */
+    public Timer(Controller controller, Museum museum){
+        this.controller = controller;
+        this.museum = museum;
+    }
+    /**
+     * Method that get only the current hour.
+     * @return current_hour
+     */
+    public int getCurrentHour(){
+        return current_hour;
+    }
+
+    /**
+     * Method that get only the current minutes.
+     * @return current_mins
+     */
+    public int getCurrentMin(){
+        return current_mins;
+    }
+
+    public String getCurrentTime(){
         return current_time;
-        }
+    }
 
-        /**
-         * Method that run all the function of the Timer Thread.
-         */
-        @Override
-        public void run(){
+    /**
+     * Method that run all the function of the Timer Thread.
+     */
+    @Override
+    public void run(){
 
 
-            int num = 8;// Initialize integer num with 8 because the ticket selling start at 0800.
-                
-            // Initialize integer hours that starting from 8 to 18 which represent 
-            // the museum start selling ticket from 0800 and the museum closed at 1800.            
-            for(int hours=num; hours <= 18; hours++){
-                
-                // Initialize integer min starting from 0 to 59 representing minutes in 
-                // clock minutes start from 00 to 59.                    
-                for(int min=0; min <=59; min++){
-                     // Printing time in 24 hours system in string. e.g. 1750
-                    String time = String.format("%02d", hours)+String.format("%02d", min);
-                    try {
-                        current_time = time;
+        int num = 8;// Initialize integer num with 8 because the ticket selling start at 0800.
+
+        // Initialize integer hours that starting from 8 to 18 which represent
+        // the museum start selling ticket from 0800 and the museum closed at 1800.
+        for(int hours=num; hours <= 18; hours++){
+
+            // Initialize integer min starting from 0 to 59 representing minutes in
+            // clock minutes start from 00 to 59.
+            for(int min=0; min <=59; min++){
+                // Printing time in 24 hours system in string. e.g. 1750
+                String time = String.format("%02d", hours)+String.format("%02d", min);
+                try {
+                    current_time = time;
+                    Platform.runLater(()->{
                         controller.setTimerTxt(time);
-                        current_hour = hours;
-                        current_mins = min;
-                        if(hours==9&&min==0){// The time museum open at 0900
-                            controller.changeMuseumStatus("Open");
-                            System.out.println(time+" Museum is opened");
-                        }else if(hours==17&&min==45){// The time museum about to close at 1750
-                            controller.changeMuseumStatus("Closing");
-                            System.out.println(time+" Museum is closing");
-                        } else if(hours==18&&min==0){// The time museum close at 1800
-                            controller.changeMuseumStatus("Closed");
-                            System.out.println(time+" Museum is closed");
-                        }
+                        controller.setCurrentTxt(museum.getCurrentCapacity());
+                    });
 
-                        Thread.sleep(200);
-                    }catch (InterruptedException e){
-                        e.printStackTrace();
+                    current_hour = hours;
+                    current_mins = min;
+                    if(hours==9&&min==0){// The time museum open at 0900
+                        Platform.runLater(()->{
+                            controller.changeMuseumStatus("Open");
+                        });
+
+                        System.out.println(time+" Museum is opened");
+                    }else if(hours==17&&min==45){// The time museum about to close at 1750
+                        Platform.runLater(()->{
+                            controller.changeMuseumStatus("Closing");
+                        });
+
+                        System.out.println(time+" Museum is closing");
+                    } else if(hours==18&&min==0){// The time museum close at 1800
+                        Platform.runLater(()->{
+                            controller.changeMuseumStatus("Closed");
+                        });
+
+                        System.out.println(time+" Museum is closed");
                     }
 
+                    Thread.sleep(200);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
                 }
-            }
 
+            }
         }
+
+    }
 }
